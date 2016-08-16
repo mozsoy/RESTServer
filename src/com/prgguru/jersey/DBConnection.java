3be69ec88f209d55a81915a00efd2e738a574307
Metehan <metehan.ozsoy@gmail.com>
2016-08-15 22:48:58 -0700

    havng to where

--- src/com/prgguru/jersey/DBConnection.java
+++ src/com/prgguru/jersey/DBConnection.java
@@ -203,7 +203,7 @@ public class DBConnection {
     	PreparedStatement ps;
 		try {
 			ps = dbConn
-					.prepareStatement("SELECT url,freq  FROM urls  HAVING freq>50 ORDER BY freq DESC");
+					.prepareStatement("SELECT url,freq  FROM urls WHERE freq>50 ORDER BY freq DESC");
 			ResultSet rs = ps.executeQuery();
 	    	while(rs.next()) {	    		
 	    		String url = rs.getString("url");
