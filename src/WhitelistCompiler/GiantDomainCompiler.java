6a411bf069d2f8f76034ebbd106786ff439fbc98
Metehan <metehan.ozsoy@gmail.com>
2016-08-15 20:58:15 -0700

    fixed concurrent modification error

--- src/WhitelistCompiler/GiantDomainCompiler.java
+++ src/WhitelistCompiler/GiantDomainCompiler.java
@@ -9,25 +9,13 @@ public class GiantDomainCompiler {
 	/**
 	 * For giant domaoins Google, Facebook, Amazon, Twitter 
 	 * whitelist everything except there is 'ad' in the url.
+	 * However this class is under development. 
 	 * 
 	 * @param urlMap
 	 * @return
 	 */
 	public void filterGiantDomains(HashMap<String,Long> urlMap) {
-		Set<String> urlSet = urlMap.keySet();
-    	Iterator<String> itr = urlSet.iterator();
-    	itr = urlSet.iterator(); // Reset iterator
-        while (itr.hasNext())
-        {
-            String s = itr.next();
-            String sLowerCase = s.toLowerCase();
-            if ((sLowerCase.contains("facebook") 
-            		|| sLowerCase.contains("google") 
-            		|| sLowerCase.contains("amazon") 
-            		|| sLowerCase.contains("twitter"))
-            		&& !sLowerCase.contains("ad")) {
-                urlMap.remove(s);
-            }
-        } 
+
+ 
 	}
 }
