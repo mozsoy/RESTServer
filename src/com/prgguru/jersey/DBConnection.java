28e0a3ae52a04e6fbbdd3031b7b926b3fbcba096
Metehan <metehan.ozsoy@gmail.com>
2016-08-24 21:53:30 -0700

    remove LIMIT 1000 from selectUrl

--- src/com/prgguru/jersey/DBConnection.java
+++ src/com/prgguru/jersey/DBConnection.java
@@ -177,7 +177,7 @@ public class DBConnection {
     	PreparedStatement ps;
 		try {
 			ps = dbConn
-					.prepareStatement("SELECT url,freq,timestamp  FROM urls ORDER BY freq DESC LIMIT 1000");
+					.prepareStatement("SELECT url,freq,timestamp  FROM urls ORDER BY freq DESC");
 			ResultSet rs = ps.executeQuery();
 	    	while(rs.next()) {	    		
 	    		String url = rs.getString("url");
